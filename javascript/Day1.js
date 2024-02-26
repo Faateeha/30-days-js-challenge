bb22686f92bddf905109488b425c707c86fcdd3c
Adeyemi Fathia <146019245+Faateeha@users.noreply.github.com>
2024-02-26 22:41:17 +0100

    Add files via upload

---
+++ javascript/Day1.js
@@ -0,0 +1,10 @@
+//Write a function createHelloWorld. it should return a new function that always returns "Hello World"
+
+const createHelloWorld = function() {
+    return function() {
+        return "Hello World";
+    }
+};
+
+
+
