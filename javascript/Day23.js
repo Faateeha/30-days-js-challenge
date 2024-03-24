00e8923240f5fc11986b62853188bd2c6b16dddf
Adeyemi Fathia <fathiaomolara02@gmail.com>
2024-03-24 20:27:19 +0100

    Day23

---
+++ javascript/Day23.js
@@ -0,0 +1,7 @@
+/*Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements, it should return -1.*/
+Array.prototype.last = function() {
+   if(this.length) {
+         return this[this.length - 1];
+   }
+    return -1;
+};
